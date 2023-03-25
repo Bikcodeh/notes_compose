135b6b221710f7c04a299461fa592555a773c6a2
Bikcode <bikcodeh@gmail.com>
2023-03-25 17:24:14 -0500

    rename table name

--- app/src/main/java/com/bikcodeh/notes_compose/data/local/database/entity/ImageToUpload.kt
+++ app/src/main/java/com/bikcodeh/notes_compose/data/local/database/entity/ImageToUpload.kt
@@ -3,7 +3,7 @@ package com.bikcodeh.notes_compose.data.local.database.entity
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "images_to_upload")
+@Entity(tableName = "image_to_upload")
 data class ImageToUpload(
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
